49a120b69196fc8f23e1eff9cab593ce269fad6d
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2021-01-24 16:27:27 -0300

    iniciando estudos com JS

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive portanto console com C maiúsculo apresentará erro.
+
+const idade = 25;
+const nome = "Jônatas";
+
+//não fazer
+//mes = "Janeiro"; JS permite a declaração mas não é uma boa prática
